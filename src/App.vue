130a2042606bc90404c093d8becd9676e805ccd1
valentin <valentin.escamilla@outlook.fr>
2018-01-15 14:46:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ 
+</template>
+
+<script>
+import Signin from './components/Signin'
+
+
+export default {
+  name: 'App',
+  components: {
+    Signin
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Signin.vue
@@ -0,0 +1,23 @@
+<template>
+	<form action="/signin">
+  Login:<br>
+  <input type="text" name="login" value="login">
+  <br>
+  Password:<br>
+  <input type="text" name="password" value="password">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+</template>
+
+<script>
+
+export default {
+  name: 'Signin',
+  data () {
+    return {
+ 
+    }
+  }
+}
+</script>
