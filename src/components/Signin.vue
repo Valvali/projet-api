47d431b337885f35ee116392794c2a2a9566b251
Charles BISELX <cha.biselx@gmail.com>
2018-01-16 15:41:33 +0100

    add route to signin & home ...

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,22 +1,25 @@
 <template>
 	<form action="/signin">
-  Login:<br>
-  <input type="text" name="login" value="login">
-  <br>
-  Password:<br>
-  <input type="text" name="password" value="password">
-  <br><br>
-  <input type="submit" value="Submit">
-</form> 
+    <label for="name">Indiquer votre nom complet</label><br>
+		<input type="text" name="name" placeholder="your name"><br>
+
+		<label for="email">Indiquer votre nom complet</label><br>
+		<input type="email" name="email" placeholder="your name"><br>
+
+		<label for="password">Indiquer votre nom complet</label><br>
+		<input type="text" name="password" placeholder="your name"><br>
+
+		<button type="submit" @click="NewUser" name="button">valider</button>
+	</form>
 </template>
 
 <script>
-
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'Signin',
   data () {
     return {
- 
+
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue 				from 'vue'
+import VueRouter 	from 'vue-router'
+import Home 			from '../components/Home'
+import Signin 		from '../components/Signin'
 
 
 Vue.use(VueRouter)
@@ -7,6 +9,6 @@ Vue.use(VueRouter)
 export default new VueRouter({
 	routes: [
 		{path: '/', component: Home},
-		
+		{path: '/signin', name: 'signin', component: Signin},
 	]
 })
