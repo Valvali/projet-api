31267cc98c624369c4f8486076260097c9cd3883
Charles BISELX <cha.biselx@gmail.com>
2018-01-16 17:19:18 +0100

    add signup

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue 				from 'vue'
 import VueRouter 	from 'vue-router'
 import Home 			from '../components/Home'
 import Signin 		from '../components/Signin'
+import Signup 		from '../components/Signup'
+
 
 
 Vue.use(VueRouter)
@@ -10,5 +12,7 @@ export default new VueRouter({
 	routes: [
 		{path: '/', component: Home},
 		{path: '/signin', name: 'signin', component: Signin},
+		{path: '/signup', name: 'signup', component: Signup},
+
 	]
 })
