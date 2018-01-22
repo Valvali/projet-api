86943a6d7e9f304a2645548350522167b8397b65
valentin <valentin.escamilla@outlook.fr>
2018-01-22 15:39:25 +0100

    Changes to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,41 @@
-import Vue 				from 'vue'
-import VueRouter 	from 'vue-router'
-import Home 			from '../components/Home'
-import Signin 		from '../components/Signin'
-import Signup 		from '../components/Signup'
+import Vue from 'vue'
+import Router from 'vue-router'
+import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
+import Home from '@/components/Home'
+import store from '@/store'
 
+Vue.use(Router)
 
+export const router = new Router({
+  routes: [
+    {
+    	path: '/',
+    	name: 'home',
+    	component: Home,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: Signin,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    }
+  ]
+})
 
-Vue.use(VueRouter)
-
-export default new VueRouter({
-	routes: [
-		{path: '/', component: Home},
-		{path: '/signin', name: 'signin', component: Signin},
-		{path: '/signup', name: 'signup', component: Signup},
-
-	]
+router.beforeEach((to, from, next) => {
+	if(to.name != 'signin' && ! store.getters['auth/isConnected']) {
+    next({name: 'signin', query: {redirect: to.fullPath}})
+  }
+  else if (to.name == 'signin' && store.getters['auth/isConnected']) {
+    console.log("test test test")
+  	next({name: 'home'})
+  }
+  else {
+    next()
+  }
 })
