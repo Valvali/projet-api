604f84ec9f676798dacc5bd212d5803f94361046
Charles BISELX <cha.biselx@gmail.com>
2018-01-23 11:55:59 +0100

    start to show list of channels

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,6 +2,7 @@
 	<div class="content">
 		<div class="right">
 			<h1>Salons :</h1>
+			<button class="actualiseChannels" @click="getChannel">Actualiser</button>
 		</div>
 		<div class="left">
 			<h1>Discussion</h1>
@@ -11,15 +12,35 @@
 </template>
 
 <script>
+import api from '@/services/api'
+import ls  from '@/services/ls'
+
 import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'Chat',
   data () {
     return {
-
+			channels:[],
     }
   },
 	methods: {
+		getChannel () {
+			this.channels = [];
+		  api.get('/channels?token='+ls.get(['token'])).then(response => {
+				// success callback
+				this.channels.push([response.label, response.topic, response.updated_at])
+
+				console.log(this.channels);
+
+
+			}, response => {
+				// error callback
+				console.log("error = "+response.message)
+				console.log(ls.get(['token']))
+
+
+			})
+		}
 
   },
 }
@@ -40,5 +61,6 @@ export default {
 	}
 	.content{
 		height: 100%;
+		text-align: left;
 	}
 </style>
