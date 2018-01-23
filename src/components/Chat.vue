587b27161ba798cb9f966f973456677e25b81aee
Charles BISELX <cha.biselx@gmail.com>
2018-01-23 17:04:45 +0100

    add css and a draft fot futur function

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,11 +1,29 @@
 <template>
 	<div class="content">
-		<div class="right">
+		<div class="left">
 			<h1>Salons :</h1>
 			<button class="actualiseChannels" @click="getChannel">Actualiser</button>
+			<ul>
+				<li v-for="(item) in channels">
+					<div class="channelTitle" id="item[3]" v-on:click="getContentChannel('item[3]','item[0]' ,'item[1]')">
+						<h4>{{item[0]}}</h4>
+						<p>{{item[1]}}</p>
+
+					</div>
+				</li>
+			</ul>
 		</div>
-		<div class="left">
+		<div class="right">
 			<h1>Discussion</h1>
+			<h2>{{title}}</h2>
+			<p><strong>Topic : </strong>{{topic}}</p>
+			<div class="chatContent">
+				{{content}}
+			</div>
+			<form class="inputChat"  method="post">
+				<input class="msg" type="text" name="" >
+				<button type="submit" name="button">Send</button>
+			</form>
 		</div>
 	</div>
 
@@ -20,12 +38,19 @@ export default {
   name: 'Chat',
   data () {
     return {
-			channels:[],
+			channels:[["label1","topic","date","qslkdjvnq"],["label2","topic","date","sqhbdvq"]],
+			content: "cliquez sur un salon pour commencer à discuter",
+			title : "",
+			topic : ""
     }
   },
 	methods: {
 		getChannel () {
+			this.content = ""
+			this.topic = ""
+			this.title = ""
 			this.channels = [];
+
 		  api.get('/channels?token='+ls.get(['token'])).then(response => {
 				// success callback
 				this.channels.push([response.label, response.topic, response.updated_at])
@@ -37,9 +62,25 @@ export default {
 				// error callback
 				console.log("error = "+response.message)
 				console.log(ls.get(['token']))
+			})
+		},
+
+		getContentChannel (IDChannel , titleName, topicName) {
+			api.get('/channels/'+IDChannel+'/posts?token=:token'+ls.get(['token'])).then(response => {
+				// success callback
+				this.content = "blablabla"
+				this.topic = topicName
+				this.title = titleName
+
+			}, response => {
+				// error callback
+				this.content += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel nunc vel massa placerat imperdiet ac ut libero. Nunc bibendum elit ac nibh gravida, non efficitur est mattis. Nunc pellentesque, leo nec tempor fermentum, justo magna ullamcorper mi, eget feugiat augue leo sit amet tortor. Sed aliquam, orci eget dapibus condimentum, ante est commodo diam, in commodo enim quam vel felis. Mauris sed purus eget nunc dictum sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi sollicitudin in mi id accumsan. Donec sem massa, feugiat sed volutpat ut, volutpat quis nibh. Curabitur ac lorem sed eros eleifend condimentum ac id tellus. Praesent porta eu ipsum sed cursus. Sed odio turpis, faucibus nec malesuada eget, sollicitudin sit amet felis. Quisque bibendum sapien vel laoreet semper. Fusce ac est non mi gravida eleifend vel vitae enim. Curabitur eget orci vestibulum neque malesuada ornare. Aliquam  hendrerit non "
+				this.topic = topicName
+				this.title = titleName
 
 
 			})
+
 		}
 
   },
@@ -47,20 +88,53 @@ export default {
 </script>
 
 <style scoped>
-	.right{
+	.left{
 		display: inline-block;
-		height: 100%;
+		height: 88%;
 		margin: -10px 0 -10px -10px;
 		padding: 10px;
 		width: 20%;
 		min-width: 175px;
 		background-color: pink;
 	}
-	.left{
+	.right{
 		display: inline-block;
+		vertical-align: top;
+		width: 78%;
+	}
+	.right p{
+		margin: 5px;
 	}
 	.content{
 		height: 100%;
 		text-align: left;
 	}
+	ul {
+		overflow-y: scroll;
+		padding-left: 0;
+	  list-style-type: none;
+		max-height: 680px;;
+	}
+	.channelTitle{
+		border-style: dotted;
+		border-width: thin;
+		padding-left: 10px;
+		margin-bottom: 2px;
+	}
+	.channelTitle:hover{
+		cursor: pointer;
+		background-color: rgba(153, 204, 255, 0.90);
+	}
+	.channelTitle h4{
+		line-height: 2px;
+	}
+	.chatContent{
+	overflow-y: scroll;
+	height: 600px;
+	width: 100%
+	}
+	.msg{
+		width: 90%
+	}
+
 </style>
